32b34041eca0d5d62f5d6d45d340e26a6e082262
Liel <69732709+lielgut@users.noreply.github.com>
2021-03-24 00:40:20 +0200

    add frames to MainWindow, ViewModels and Views

---
+++ ViewModels/ViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace ex1.ViewModels
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void PropertyChangedNotify(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
--- Views/ControlsView.xaml.cs
+++ Views/ControlsView.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,21 +10,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using ex1.ViewModels;
 
-namespace ex1
+namespace ex1.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ControlsView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ControlsView : Page
     {
-        MainViewModel vm;
-        public MainWindow()
+        public ControlsView()
         {
             InitializeComponent();
-            vm = new MainViewModel();
-            DataContext = vm;
         }
     }
 }
