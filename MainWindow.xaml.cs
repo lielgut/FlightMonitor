17e96f66f3cccdd53219c76771a6644e14e57c74
Liel <69732709+lielgut@users.noreply.github.com>
2021-03-29 01:26:14 +0300

    fix main window constructor

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,12 +27,22 @@ namespace ex1
         public MainWindow()
         {
             fc = new FlightControl();
-            ConfigWindow cw = new ConfigWindow(this,fc);
+            ConfigWindow cw = new ConfigWindow(this, fc);
             cw.Show();
-            InitializeComponent();                                    
-            playerView.PlayerVM = new PlayerViewModel(fc);
-            dataView.DataVM = new DataViewModel(fc);
-            reserachView.ResearchVM = new ResearchViewModel(fc);
+            InitializeComponent();
+
+            PlayerViewModel pvm = new PlayerViewModel(fc); ;
+            playerView.PlayerVM = pvm;
+            playerView.DataContext = pvm;
+
+            DataViewModel dvm = new DataViewModel(fc); ;
+            dataView.DataVM = dvm;
+            dataView.DataContext = dvm;
+
+            ResearchViewModel rvm = new ResearchViewModel(fc); ;
+            researchView.ResearchVM = rvm;
+            researchView.DataContext = rvm;
+
             Visibility = Visibility.Hidden;
         }
 
